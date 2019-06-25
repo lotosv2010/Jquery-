6d1a2be9cf8f599a83f6231070ef7a6dc3297cc9
高文斌 <wenbingao@sfmail.sf-express.com>
2019-06-25 16:56:37 +0800

    新增文件

---
+++ Jquery-SourceCode-Analysis/梳理JQ的框架.js
@@ -0,0 +1,11 @@
+(function() {
+    // (21, 94) 定义了一些变量和函数 jQuery = function(){}
+
+    // (96, 283) 给JQ对象，添加一些方法和属性
+
+    // (258, 347) extend:JQ的继承方法
+
+    // (349, 817) jQuery.extend(): 扩展一些工具方法
+
+    // (8826) window.jQuery = window.$ = jQuery
+})()
